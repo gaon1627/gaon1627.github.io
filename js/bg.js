d91b0c59ac55f467f6541eaa1966b5e38d97a245
gaon1627 <67174525+gaon1627@users.noreply.github.com>
2020-06-20 10:57:26 +0900

    Update bg.js

--- js/bg.js
+++ js/bg.js
@@ -2,7 +2,10 @@ const UNSPLASH_URL = "https://source.unsplash.com//random";
 const body = document.querySelector("body");
 
 function paintBg(){
-  body.style.backgroundImage = `url(${UNSPLASH_URL})`;
+  body.style.backgroundImage = `url(${UNSPLASH_URL})`
+  body.style.backgroundSize = `auto`;
+  body.style.backgroundPosition = `center top`;
+  body.style.backgroundRepeat = `no-repat`;
 }
 
 function init(){
