7a8962ab6d07c922b8df38f9d60a776fd9a946bb
gaon1627 <67174525+gaon1627@users.noreply.github.com>
2020-06-20 10:30:39 +0900

    Create bg.js

---
+++ js/bg.js
@@ -0,0 +1,11 @@
+const UNSPLASH_URL = "https://source.unsplash.com//random";
+const body = document.querySelector("body");
+
+function paintBg(){
+  body.style.backgroundImage = `url($UNSPLASH_URL})`;
+}
+
+function init(){
+  paintBg();
+}
+init();
